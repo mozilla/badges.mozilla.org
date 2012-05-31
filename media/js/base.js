43846efb27beff51714cbdea978b0fc0c57487b5
Les Orchard <me@lmorchard.com>
2012-05-30 22:12:04 -0400

    Set up Google Analytics, add edge padding to QR code in PDFs

--- media/js/base.js
+++ media/js/base.js
@@ -1,6 +1,18 @@
 /**
  * Base JS enhancements
  */
+
+// Set up Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-32268013-1']);
+_gaq.push(['_setDomainName', 'badg.us']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 $(document).ready(function () {
 
     $('.browserid-signin').click(function (e) {
