f390d5efba194ac35dfd504280de5c614dc0a835
Les Orchard <me@lmorchard.com>
2013-01-24 11:00:11 -0500

    Accept additional markup, and allow href on <a> (doh!)

--- badgus/base/helpers.py
+++ badgus/base/helpers.py
@@ -1,27 +1,51 @@
 from jingo import register
 import jinja2
 
+from django.conf import settings
 
-# TODO: Allow configurable whitelists
-ALLOWED_TAGS = [
-    'a', 'abbr', 'br', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'img', 'li',
-    'ol', 'p', 'strong', 'ul'
-]
 
-ALLOWED_ATTRIBUTES = {
-    "img": ["width", "height", "src"]
-}
+if hasattr(settings, 'BLEACH_ALLOWED_TAGS'):
+    ALLOWED_TAGS = settings.BLEACH_ALLOWED_TAGS
+else:
+    ALLOWED_TAGS = [
+        'a', 'abbr', 'br', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'img', 'li',
+        'ol', 'p', 'strong', 'ul', 'dl', 'dt', 'dd',
+        'pre', 'code', 'cite',
+        'dl', 'dt', 'dd', 'small', 'sub', 'sup', 'u', 'strike', 'samp',
+        'table', 'tbody', 'thead', 'tfoot', 'tr', 'th', 'td', 'colgroup', 'col',
+    ]
+
+if hasattr(settings, 'BLEACH_ALLOWED_ATTRIBUTES'):
+    ALLOWED_ATTRIBUTES = settings.BLEACH_ALLOWED_ATTRIBUTES
+else:
+    ALLOWED_ATTRIBUTES = {
+        '*': ['style'],
+        'img': ['width', 'height', 'src', 'alt'],
+        'a': ['href', 'title'],
+    }
+
+if hasattr(settings, 'BLEACH_ALLOWED_STYLES'):
+    ALLOWED_STYLES = settings.BLEACH_ALLOWED_STYLES
+else:
+    ALLOWED_STYLES = [
+        'width', 'height'
+    ]
+
 
 @register.filter
-def bleach_markup(val):
+def bleach_markup(val, nltobr=True, linkify=True):
     """Template filter to linkify and clean content expected to allow HTML"""
     try:
         import bleach
-        val = val.replace('\n', '<br />')
-        val = bleach.linkify(val)
+        if nltobr:
+            val = val.replace('\n', '<br />')
+        if linkify:
+            val = bleach.linkify(val)
         val = bleach.clean(val,
                            tags=ALLOWED_TAGS,
-                           attributes=ALLOWED_ATTRIBUTES)
+                           attributes=ALLOWED_ATTRIBUTES,
+                           styles=ALLOWED_STYLES)
         return jinja2.Markup(val)
     except ImportError:
+        # If bleach is unavailable, just silently punt.
         return val
